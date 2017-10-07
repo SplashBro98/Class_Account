27031d85c43f7b254741a44aadc389fe999cf2c1
SplashBro98 <Moz.Vana@yandex.ru>
2017-10-07 16:02:59 +0300

    Initial commit

---
+++ src/Liverpool/Account.java
@@ -0,0 +1,143 @@
+package Liverpool;
+
+
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class Account implements Comparable<Account>{
+    private Client fio;
+    private String nomer;
+    private Valyuta currency;
+    private double proc;
+    private double sum;
+
+    enum Valyuta{rubly, dollars, euro, pounds};
+
+    public static int counter = 1;
+    public Client getFio() {
+        return fio;
+    }
+    public String getNomer() {
+        return nomer;
+    }
+    public String getCurrency() {
+        switch(this.currency){
+            case rubly: return "rubly";
+            case dollars: return "dollars";
+            case euro: return "euro";
+            case pounds: return "pounds";
+            default:
+                return "none";
+        }
+    }
+    public double getProc() {
+        return proc;
+    }
+    public double getSum() {
+        return sum;
+    }
+
+    public Valyuta Otvorot(String s){
+        switch (s){
+            case "rubly": return Valyuta.rubly;
+            case "dollars": return Valyuta.dollars;
+            case "euro": return Valyuta.euro;
+            case "pounds": return Valyuta.pounds;
+            default: return Valyuta.rubly;
+        }
+    }
+
+
+    public void setProc(float proc) {
+        this.proc = proc;
+    }
+    public void setSum(int sum) {
+        this.sum = sum;
+    }
+
+    public Account(Client fio) {
+        this.fio = new Client(fio.getName(),fio.getSurname());
+        String s = String.valueOf(fio.getName().charAt(0)) + String.valueOf(fio.getSurname().charAt(0));
+        this.nomer = s + String.valueOf(counter);
+        counter++;
+        this.proc = 0;
+        this.sum = 0;
+        this.currency = null;
+    }
+
+    public Account(Client fio, Valyuta currency, double proc, double sum) {
+        this.fio = new Client(fio.getName(),fio.getSurname());
+
+        String s = String.valueOf(fio.getName().charAt(0)) + String.valueOf(fio.getSurname().charAt(0));
+        this.nomer = s + String.valueOf(counter);
+        counter++;
+        this.currency = currency;
+        this.proc = proc;
+        this.sum = sum;
+    }
+
+    public Account(String s){
+        String [] d = s.split(";");
+        int j = d[0].indexOf(' ');
+        String s1 = d[0].substring(0,j);
+        String s2 = d[0].substring(j+1,d[0].length());
+        this.fio = new Client(s1,s2);
+        String s3 = String.valueOf(fio.getName().charAt(0)) + String.valueOf(fio.getSurname().charAt(0));
+        this.nomer = s3 + String.valueOf(counter);
+        counter++;
+        this.sum = Double.parseDouble(d[3]);
+        this.proc = Double.parseDouble(d[2]);
+        this.currency = Otvorot(d[1]);
+    }
+
+    public void Take(int sum) throws MyException{
+        if(sum>this.sum)
+            throw new MyException("This sum is too big. Insufficient funds on the account");
+        this.sum -=sum;
+    }
+    public void Add(int sum) throws MyException{
+        if(sum < 0)
+            throw new MyException("Wrong parametr");
+        this.sum +=sum;
+    }
+    public void Accrue_Procents(int days)throws MyException{
+        if(days < 0)
+            throw new MyException("Wrong parametr");
+        double f = this.sum * this.proc;
+        f =f / 360 * days;
+        this.sum +=f;
+    }
+
+    @Override
+    public int compareTo(Account o) {
+        System.out.println("Please, choose the field: ");
+        System.out.println("1 - Surname");
+        System.out.println("2 - Nomer");
+        System.out.println("3 - Proc");
+        System.out.println("4 - Sum");
+        Scanner in = new Scanner(System.in);
+        switch(in.nextInt()){
+            case 1: return this.fio.getSurname().equals(o.fio.getSurname()) ? 1 : -1;
+            case 2: String s1 = this.nomer.substring(2,this.nomer.length());
+                    String s2 = o.nomer.substring(2, o.nomer.length());
+                    return s1.equals(s2) ? 1 : -1;
+            case 3: return this.proc > o.proc ? 1 : -1;
+            case 4: return this.sum > o.sum ? 1 : -1;
+            default: return 1;
+        }
+
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Account: " +
+                 fio.getName() + " " + fio.getSurname()+ " " +
+                "," + nomer  +
+                ", " + currency +
+                ", " + proc +
+                ", sum: " + sum;
+    }
+
+}
---
+++ src/Liverpool/Test.java
@@ -0,0 +1,71 @@
+package Liverpool;
+
+
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+
+import java.io.*;
+import java.util.Arrays;
+
+import static Liverpool.Account.Valyuta.dollars;
+import static Liverpool.Account.Valyuta.euro;
+import static Liverpool.Account.Valyuta.rubly;
+import static Liverpool.Account.Valyuta.pounds;
+
+public class Test {
+
+    public static void main(String[] args) throws IOException, MyException {
+        try {
+
+
+            FileWriter out = new FileWriter(new File("D:\\Проекты по проге\\3-й семестр\\Прога\\Лаба№5_13\\output.txt"));
+            //FileReader in = new FileReader(new File("D:\\Проекты по проге\\3-й семестр\\Прога\\Лаба№5_13\\input.txt"));
+            FileInputStream in = new FileInputStream(new File("D:\\Проекты по проге\\3-й семестр\\Прога\\Лаба№5_13\\input.txt"));
+            InputStreamReader f1 = new InputStreamReader(in);
+            BufferedReader buf = new BufferedReader(f1);
+            Client Alina = new Client("Alina","Karpovich");
+            Client Diana = new Client("Diana", "Kulich");
+            Account user_1 = new Account(Alina,dollars,0.1,100.0);
+            Account user_2 = new Account(Diana,euro,0.15,120.0);
+           // System.out.println(user_1.toString());
+           //System.out.println(user_2.toString());
+//            user_1.Take(30);
+//            System.out.println(user_1.toString());
+//            user_1.Add(370);
+//            System.out.println(user_1.toString());
+            //user_1.Accrue_Procents(40);
+            //System.out.println(user_1.toString());
+            String s = buf.readLine();
+            Account user_3 = new Account(s);
+            //System.out.println(user_3.toString());
+
+           // System.out.println(user_1.compareTo(user_3));
+
+            Account [] acc = new Account[3];
+
+            acc[0] = user_3;
+            acc[1] = user_1;
+            acc[2] = user_2;
+            for (int i = 0; i < acc.length; i++) {
+                System.out.println(acc[i].toString());
+            }
+            Arrays.sort(acc);
+
+            for (int i = 0; i < acc.length; i++) {
+                System.out.println(acc[i].toString());
+            }
+
+
+            out.write(user_1.toString() + "\n");
+            out.write(user_2.toString());
+            out.flush();
+
+
+        }
+        catch (IOException e){
+            e.getMessage();
+        }
+
+
+
+    }
+}
